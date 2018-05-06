6f75e2fc5f090e2c12209b1fb705b9e368ffa596
vlado78 <vladimir.budac@gmail.com>
2018-05-06 14:12:49 +0200

    dodane veze

--- zadatak_5.sql
+++ zadatak_5.sql
@@ -6,28 +6,30 @@ use muzej;
 
 create table izlozba (
 sifra int not null primary key auto_increment,
-dijela int,
-kustos int,
-sponzor int
+dijela int
 );
 
 create table dijela(
 sifra int not null primary key auto_increment,
 naziv varchar(20)
-
 );
 
 create table kustos(
 sifra int not null primary key auto_increment,
+izlozba int,
 ime varchar(20),
 prezima varchar(20)
 );
 
 create table sponzor(
 sifra int not null primary key auto_increment,
+izlozba int,
 ime varchar (20),
 prezime varchar (20)
 );
 
 
 alter table izlozba add foreign key (dijela) references dijela(sifra);
+alter table kustos add foreign key (izlozba) references izlozba(sifra);
+alter table sponzor add foreign key (izlozba) references izlozba(sifra);
+
